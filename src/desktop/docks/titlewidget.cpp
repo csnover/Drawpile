69c5b4fb7159d4cb71d5f1f66102d40e268ed17e
Colin Snover <github.com@zetafleet.com>
2023-02-08 11:17:53 -0600

    Fix wrong dock icon sizes on Windows

--- src/desktop/docks/titlewidget.cpp
+++ src/desktop/docks/titlewidget.cpp
@@ -24,7 +24,13 @@
 #include <QLabel>
 #include <QAbstractButton>
 #include <QPainter>
-#include <QStyleOptionButton>
+#ifndef QT_NO_STYLE_PROXY
+#include <QProxyStyle>
+#endif
+#include <QStyle>
+#include <QStyleOptionToolButton>
+#include <QStylePainter>
+#include <Qt>
 
 namespace docks {
 
@@ -41,9 +47,13 @@ public:
 	QSize sizeHint() const override;
 	QSize minimumSizeHint() const override { return sizeHint(); }
 
+	bool event(QEvent *event) override;
 	void enterEvent(compat::EnterEvent *event) override;
 	void leaveEvent(QEvent *event) override;
 	void paintEvent(QPaintEvent *event) override;
+private:
+	QSize dockButtonIconSize() const;
+	mutable int m_iconSize = -1;
 };
 
 TitleWidget::Button::Button(const QIcon &icon, QWidget *parent)
@@ -54,14 +64,57 @@ TitleWidget::Button::Button(const QIcon &icon, QWidget *parent)
 	setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 }
 
+bool TitleWidget::Button::event(QEvent *event)
+{
+	switch (event->type()) {
+	case QEvent::StyleChange:
+	case QEvent::ScreenChangeInternal:
+		m_iconSize = -1;
+		break;
+	default:
+		break;
+	}
+	return QAbstractButton::event(event);
+}
+
+static inline bool isWindowsStyle(const QStyle *style)
+{
+	// Note: QStyleSheetStyle inherits QWindowsStyle
+	const QStyle *effectiveStyle = style;
+
+	// Qt checked for its internal QStyleSheetStyle here to get its baseStyle,
+	// which is impossible in this implementation
+#if !defined(QT_NO_STYLE_PROXY)
+	if (style->inherits("QProxyStyle"))
+		effectiveStyle = static_cast<const QProxyStyle *>(style)->baseStyle();
+#endif
+
+	return effectiveStyle->inherits("QWindowsStyle");
+}
+
+QSize TitleWidget::Button::dockButtonIconSize() const
+{
+	if (m_iconSize < 0) {
+		m_iconSize = style()->pixelMetric(QStyle::PM_SmallIconSize, nullptr, this);
+		// Dock Widget title buttons on Windows where historically limited to size 10
+		// (from small icon size 16) since only a 10x10 XPM was provided.
+		// Adding larger pixmaps to the icons thus caused the icons to grow; limit
+		// this to qpiScaled(10) here.
+		if (isWindowsStyle(style()))
+			m_iconSize = qMin((10 * logicalDpiX()) / 96, m_iconSize);
+	}
+	return QSize(m_iconSize, m_iconSize);
+}
+
 QSize TitleWidget::Button::sizeHint() const
 {
 	ensurePolished();
 
-	const int iconSize = style()->pixelMetric(QStyle::PM_SmallIconSize, nullptr, this);
-	int size = style()->pixelMetric(QStyle::PM_DockWidgetTitleBarButtonMargin, nullptr, this);
-	const QSize sz = icon().actualSize(QSize(iconSize, iconSize));
-	size += qMax(sz.width(), sz.height());
+	int size = 2*style()->pixelMetric(QStyle::PM_DockWidgetTitleBarButtonMargin, nullptr, this);
+	if (!icon().isNull()) {
+		const QSize sz = icon().actualSize(dockButtonIconSize());
+		size += qMax(sz.width(), sz.height());
+	}
 
 	return QSize(size, size);
 }
@@ -80,15 +133,32 @@ void TitleWidget::Button::leaveEvent(QEvent *event)
 
 void TitleWidget::Button::paintEvent(QPaintEvent *)
 {
-	QPainter p(this);
+	QStylePainter p(this);
 
-	icon().paint(
-		&p,
-		QRect(1, 1, width()-2, height()-2),
-		Qt::AlignCenter,
-		underMouse() ? QIcon::Active : QIcon::Normal,
-		isDown() ? QIcon::On : QIcon::Off
-		);
+	QStyleOptionToolButton opt;
+	opt.initFrom(this);
+	opt.state |= QStyle::State_AutoRaise;
+
+	if (style()->styleHint(QStyle::SH_DockWidget_ButtonsHaveFrame, nullptr, this)) {
+		if (isEnabled() && underMouse() && !isChecked() && !isDown())
+			opt.state |= QStyle::State_Raised;
+		if (isChecked())
+			opt.state |= QStyle::State_On;
+		if (isDown())
+			opt.state |= QStyle::State_Sunken;
+		p.drawPrimitive(QStyle::PE_PanelButtonTool, opt);
+	} else if (isDown() || isChecked()) {
+		// no frame, but the icon might have explicit pixmaps for QIcon::On
+		opt.state |= QStyle::State_On | QStyle::State_Sunken;
+	}
+
+	opt.icon = icon();
+	opt.subControls = { };
+	opt.activeSubControls = { };
+	opt.features = QStyleOptionToolButton::None;
+	opt.arrowType = Qt::NoArrow;
+	opt.iconSize = dockButtonIconSize();
+	p.drawComplexControl(QStyle::CC_ToolButton, opt);
 }
 
 TitleWidget::TitleWidget(QDockWidget *parent) : QWidget(parent)
