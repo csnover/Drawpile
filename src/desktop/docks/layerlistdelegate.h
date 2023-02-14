91088c5de0e629480bc78ce9e3f0f6e51268fae3
Colin Snover <github.com@zetafleet.com>
2023-02-13 22:32:35 -0600

    Remove hardcoded sizes from LayerListDelegate

--- src/desktop/docks/layerlistdelegate.h
+++ src/desktop/docks/layerlistdelegate.h
@@ -19,20 +19,17 @@ public:
 
 	void paint(QPainter *painter, const QStyleOptionViewItem &option,
 			const QModelIndex &index) const override;
-	QSize sizeHint(const QStyleOptionViewItem & option,
-			const QModelIndex & index ) const override;
+	QSize sizeHint(const QStyleOptionViewItem &option,
+			const QModelIndex &index) const override;
 
-	void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem & option, const QModelIndex & index ) const override;
 	bool editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option, const QModelIndex &index) override;
-
-	void setShowNumbers(bool show);
 
 signals:
 	void toggleVisibility(int layerId, bool visible);
 	void editProperties(QModelIndex index);
 
 private:
-	void drawOpacityGlyph(const QRectF& rect, QPainter *painter, float value, bool hidden, bool censored, bool group) const;
+	void drawOpacityGlyph(const QRect &rect, QPainter *painter, float value, bool hidden, bool censored, bool group) const;
 
 	QIcon m_visibleIcon;
 	QIcon m_groupIcon;
