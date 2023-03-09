f1e21a690564007696eb346872dfc4e2432205da
Colin Snover <github.com@zetafleet.com>
2023-03-09 17:43:44 -0600

    Fix identicon generation for non-BMP and joined characters

--- src/libclient/utils/identicon.cpp
+++ src/libclient/utils/identicon.cpp
@@ -13,12 +13,26 @@ QImage make_identicon(const QString &name, const QSize &size)
 	QImage image(size, QImage::Format_ARGB32_Premultiplied);
 	image.fill(color);
 
+	int len = 0;
+	bool takeNext = true;
+	for (const auto c : name) {
+		if (c.joiningType() == QChar::Joining_Causing) {
+			takeNext = true;
+			++len;
+		} else if (takeNext || c.isLowSurrogate() || c.joiningType() == QChar::Joining_Transparent) {
+			takeNext = false;
+			++len;
+		} else {
+			break;
+		}
+	}
+
 	QPainter painter(&image);
 	QFont font;
 	font.setPixelSize(size.height() * 0.7);
 	painter.setFont(font);
 	painter.setPen(Qt::white);
-	painter.drawText(QRect(QPoint(), size), Qt::AlignCenter, name.left(1));
+	painter.drawText(QRect(QPoint(), size), Qt::AlignCenter, name.left(len));
 
 	return image;
 }
