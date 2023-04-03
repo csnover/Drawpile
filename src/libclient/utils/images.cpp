9f1b9f32d07ae47c8ced7daba723d76e2b477325
Colin Snover <github.com@zetafleet.com>
2023-04-03 17:36:01 -0500

    extra compiler warnings

--- src/libclient/utils/images.cpp
+++ src/libclient/utils/images.cpp
@@ -55,17 +55,6 @@ static void appendFormats(QVector<ImageFormat> &formats, const char *title, cons
 	formats.append(format);
 }
 
-static const QVector<ImageFormat> &readableImageFormats()
-{
-	static QVector<ImageFormat> formats;
-	if(formats.isEmpty()) {
-		for(const DP_LoadFormat *lf = DP_load_supported_formats(); lf->title; ++lf) {
-			appendFormats(formats, lf->title, lf->extensions);
-		}
-	}
-	return formats;
-}
-
 static const QVector<ImageFormat> &writableImageFormats()
 {
 	static QVector<ImageFormat> formats;
